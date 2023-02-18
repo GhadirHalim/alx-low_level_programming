65ea1129989ca265eb75f10705da500175e9a949
Gaia <ghadir.halim@gmail.com>
2023-02-17 18:14:32 -0800

    handy me dows

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; b <= 'z'; c++)
+	{ 
+		if (c != 'q' && c != 'e')
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
