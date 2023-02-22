b3834527f4fe7a631382afdcecfc5688291a23f2
Gaia <ghadir.halim@gmail.com>
2023-02-21 18:47:19 -0800

    f

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - print alphabet
- *
- * Return: Always 0.
+ * print_alphabet_x10 - prints 10 times the alphabet, in lower
+ * Return : Always return 0
  */
 void print_alphabet_x10(void)
 {
